641d127db0888dd8de2c9c76a3f0e01cdf065572
Vasily Malykhin <malykhinvi@gmail.com>
2020-07-11 11:34:52 +0300

    Add babel-import-plugin

--- src/a/ComponentA.tsx
+++ src/a/ComponentA.tsx
@@ -2,11 +2,17 @@ import * as React from "react";
 import { useSelector } from "react-redux";
 import { IStore } from "../configureStore";
 import { IDataA } from "./reducerA";
+import { Button }  from "@material-ui/core";
 
 const ComponentA = () => {
   const data = useSelector<IStore, IDataA>((store) => store.a);
 
-  return <h1>{data.title}</h1>;
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <Button>Test</Button>
+    </div>
+  );
 };
 
 export default ComponentA;
