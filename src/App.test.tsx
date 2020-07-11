ba6020ca45824b3219317b394421afebcce3e8e7
Vasily Malykhin <malykhinvi@gmail.com>
2020-07-11 10:59:42 +0300

    Add App test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { describe, test, expect } from "@jest/globals";
+import App from "./App";
+
+describe("App", () => {
+  test("fake test case", () => {
+    console.log(App);
+    expect(true).toBe(true);
+  });
+});
